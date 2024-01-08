7947f553a06a69f8fc6e955d341f7538e53cc88a
waffiq <waffiqmaaroja1998@gmail.com>
2024-01-08 14:01:34 +0700

    implement test and production config

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,6 +11,13 @@ def create_app(test_config=None) -> Flask:
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
     )
 
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
