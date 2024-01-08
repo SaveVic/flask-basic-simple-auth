1042b82fbef7f99c71a7463e7cd2660e5c32b76d
waffiq <waffiqmaaroja1998@gmail.com>
2024-01-08 13:55:13 +0700

    styling api page

--- flaskr/blueprint/api.py
+++ flaskr/blueprint/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort
+from flask import Blueprint, abort, g, render_template
 import flaskr.repo.user as rep
 import flaskr.service.session as session_service
 
@@ -9,6 +9,8 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 def get_all():
     if not session_service.is_logged_in():
         return abort(404)
-    accs = rep.get_all()
-    s = '</br>'.join([f'Count: {len(accs)}'] + [str(acc) for acc in accs])
-    return s
+    users = rep.get_all()
+    g.users = users
+    # s = '</br>'.join([f'Count: {len(users)}'] + [str(user) for user in users])
+    # return s
+    return render_template('api.html')
