5dac97ee050f525db4ef9a15dc31a75e4e99aa02
waffiq <waffiqmaaroja1998@gmail.com>
2024-01-08 09:48:12 +0700

    create model and schema

---
+++ flaskr/model/schema.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Engine, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class __Base(DeclarativeBase):
+    pass
+
+
+class User(__Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(50))
+    password: Mapped[str] = mapped_column(String(255))
+    email: Mapped[str] = mapped_column(String(100))
+
+    def __repr__(self) -> str:
+        return f'User(id={self.id!r}, username={self.username!r}, email={self.email!r})'
+
+
+def init_db(engine: Engine):
+    __Base.metadata.create_all(engine)
---
+++ flaskr/model/user.py
@@ -0,0 +1,33 @@
+from typing import Union
+from flaskr.model.schema import User
+
+
+class UserModel:
+    def __init__(
+        self,
+        id: Union[int, None] = None,
+        username: Union[str, None] = None,
+        email: Union[str, None] = None,
+        password: Union[str, None] = None,
+    ) -> None:
+        self.id = id
+        self.username = username
+        self.password = password
+        self.email = email
+
+    @classmethod
+    def factory(cls, user: User):
+        return UserModel(user.id, user.username, user.email, user.password)
+
+    def to_dict(self, exclude_id=True):
+        d = {
+            'username': self.username,
+            'email': self.email,
+            'password': self.password
+        }
+        if not exclude_id:
+            d['id'] = self.id
+        return d
+
+    def __repr__(self) -> str:
+        return f'User(id={self.id!r}, username={self.username!r}, password={self.password!r} email={self.email!r})'
